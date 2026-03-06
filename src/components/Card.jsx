a1a1f4e9413511a1b756a93c0090b82c8b45e8e0
LeticiaRolando <listradajaguatirica@gmail.com>
2026-03-06 16:40:28 -0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,10 +12,10 @@ const Card = ({ title, titlecomplete, iconsvg, price, color, percentage1, percen
                     <div className="flex flex-col items-center mb-2">
                         {/* Percentage boxes */}
                         {/* 1h */}
-                        <div className="relative w-[92px] h-[55px]">
-                            <div className="absolute left-[36px] top-0 w-[20px] h-[21px] text-[#FFFFFF] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">1h</div>
-                            <div className="absolute left-0 top-[28px] w-[92px] h-[27px] bg-[rgba(241,86,83,0.2)] border border-[#F15653] rounded-[8px]">
-                                <div className="absolute left-[17px] top-[3px] w-[58px] h-[21px] text-[#F15653] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">{percentage1}%</div>
+                        <div className="relative w-23 h-13.75">
+                            <div className="absolute left-9 top-0 w-5 h-5.25 text-[#FFFFFF] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">1h</div>
+                            <div className="absolute left-0 top-7 w-23 h-6.75 bg-[rgba(241,86,83,0.2)] border border-[#F15653] rounded-[8px]">
+                                <div className="absolute left-4.25 top-0.75 w-14.5 h-[21px] text-[#F15653] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">{percentage1}%</div>
                             </div>
                             <div className="absolute left-0 top-[21px] w-[92px] h-0 border-t border-[#6B74A3]"></div>
                         </div>
