a8c124bbb67d226000a5dd139413f7786c627605
LeticiaRolando <listradajaguatirica@gmail.com>
2026-03-06 16:43:43 -0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,32 +15,32 @@ const Card = ({ title, titlecomplete, iconsvg, price, color, percentage1, percen
                         <div className="relative w-23 h-13.75">
                             <div className="absolute left-9 top-0 w-5 h-5.25 text-[#FFFFFF] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">1h</div>
                             <div className="absolute left-0 top-7 w-23 h-6.75 bg-[rgba(241,86,83,0.2)] border border-[#F15653] rounded-[8px]">
-                                <div className="absolute left-4.25 top-0.75 w-14.5 h-[21px] text-[#F15653] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">{percentage1}%</div>
+                                <div className="absolute left-4.25 top-0.75 w-14.5 h-5.25 text-[#F15653] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">{percentage1}%</div>
                             </div>
-                            <div className="absolute left-0 top-[21px] w-[92px] h-0 border-t border-[#6B74A3]"></div>
+                            <div className="absolute left-0 top-5.25 w-23 h-0 border-t border-[#6B74A3]"></div>
                         </div>
 
                         {/* 6h */}
-                        <div className="relative w-[92px] h-[55px] mt-[15px]">
-                            <div className="absolute left-[32px] top-0 w-[29px] h-[21px] text-[#FFFFFF] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">6h</div>
-                            <div className="absolute left-0 top-[28px] w-[92px] h-[27px] bg-[rgba(241,86,83,0.2)] border border-[#F15653] rounded-[8px]">
-                                <div className="absolute left-[17px] top-[3px] w-[58px] h-[21px] text-[#F15653] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">{percentage6}%</div>
+                        <div className="relative w-23 h-13.75 mt-[15px]">
+                            <div className="absolute left-8 top-0 w-7.25 h-5.25 text-[#FFFFFF] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">6h</div>
+                            <div className="absolute left-0 top-7 w-23 h-6.75 bg-[rgba(241,86,83,0.2)] border border-[#F15653] rounded-[8px]">
+                                <div className="absolute left-4.25 top-0.75 w-14.5 h-5.25 text-[#F15653] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">{percentage6}%</div>
                             </div>
-                            <div className="absolute left-0 top-[21px] w-[92px] h-0 border-t border-[#6B74A3]"></div>
+                            <div className="absolute left-0 top-5.25 w-23 h-0 border-t border-[#6B74A3]"></div>
                         </div>
 
                         {/* 12h */}
-                        <div className="relative w-[92px] h-[55px] mt-[15px]">
-                            <div className="absolute left-[32px] top-0 w-[29px] h-[21px] text-[#FFFFFF] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">12h</div>
-                            <div className="absolute left-0 top-[28px] w-[92px] h-[27px] bg-[rgba(6,217,98,0.2)] border border-[#06D962] rounded-[8px]">
-                                <div className="absolute left-[22px] top-[3px] w-[48px] h-[21px] text-[#06D962] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">{percentage12}%</div>
+                        <div className="relative w-23 h-13.75 mt-[15px]">
+                            <div className="absolute left-8 top-0 w-7.25 h-5.25 text-[#FFFFFF] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">12h</div>
+                            <div className="absolute left-0 top-7 w-23 h-6.75 bg-[rgba(6,217,98,0.2)] border border-[#06D962] rounded-[8px]">
+                                <div className="absolute left-5.5 top-0.75 w-12 h-5.25 text-[#06D962] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">{percentage12}%</div>
                             </div>
-                            <div className="absolute left-0 top-[21px] w-[92px] h-0 border-t border-[#6B74A3]"></div>
+                            <div className="absolute left-0 top-5.25 w-23 h-0 border-t border-[#6B74A3]"></div>
                         </div>
 
                         {/* 24h */}
-                        <div className="relative w-[92px] h-[55px] mt-[15px]">
-                            <div className="absolute left-[32px] top-0 w-[29px] h-[21px] text-[#FFFFFF] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">24h</div>
+                        <div className="relative w-23 h-13.75 mt-[15px]">
+                            <div className="absolute left-8 top-0 w-[29px] h-[21px] text-[#FFFFFF] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">24h</div>
                             <div className="absolute left-0 top-[28px] w-[92px] h-[27px] bg-[rgba(6,217,98,0.2)] border border-[#06D962] rounded-[8px]">
                                 <div className="absolute left-[22px] top-[3px] w-[48px] h-[21px] text-[#06D962] font-['Geist_Mono'] font-medium text-[16px] leading-[1.3] text-center">{percentage24}%</div>
                             </div>
