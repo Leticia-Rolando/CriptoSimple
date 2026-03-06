049276635f76692dff7069f59caac21628b93ff0
LeticiaRolando <listradajaguatirica@gmail.com>
2026-03-06 17:16:27 -0300

    testes

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
   return (
     <>
       <div className='fundo min-h-screen overflow-hidden'>
-        <div className='flex items justify-center'>
-          <h1 className="">oiiiii</h1>
-        </div>
-        <section className='flex flex-col items-center '>
+        <section className='flex items justify-center'>
+          <h1 className="">CriptoSimple</h1>
+        </section>
+        <section className='flex flex-col items-center'>
           <Card title={"BTC"} titlecomplete={"Bitcoin"} iconsvg={bitcoinIcon} color={"#FF7700"} price={"R$357.120,99"} percentage1={-0.39} percentage6={-1.49} percentage12={1.02} percentage24={2.39} />
           <Card title={"BTC"} titlecomplete={"Bitcoin"} iconsvg={bitcoinIcon} color={"#FF7700"} price={"R$357.120,99"} percentage1={-0.39} percentage6={-1.49} percentage12={1.02} percentage24={2.39} />
           <Card title={"BTC"} titlecomplete={"Bitcoin"} iconsvg={bitcoinIcon} color={"#FF7700"} price={"R$357.120,99"} percentage1={-0.39} percentage6={-1.49} percentage12={1.02} percentage24={2.39} />
